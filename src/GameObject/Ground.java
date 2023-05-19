8f849749b4cea07cab19a069d88de31a0391ffa9
HoènMike <hoanggg003@gmail.com>
2023-05-19 12:09:05 +0700

    fix ground get set

--- src/GameObject/Ground.java
+++ src/GameObject/Ground.java
@@ -29,7 +29,7 @@ public class Ground {
 		listGround = new ArrayList<ImageGround>();
 		for (int i = 0; i < numberOfImageGround; i++) {
 			ImageGround imageGround = new ImageGround();
-			imageGround.xPosition = i * ground1.getWidth();
+			imageGround.setXPosition(i * ground1.getWidth());
 			setImageGround(imageGround);
 			listGround.add(imageGround);
 		}
@@ -38,35 +38,35 @@ public class Ground {
 	public void update() {
 		Iterator<ImageGround> itr = listGround.iterator();
 		ImageGround firstGround = itr.next();
-		firstGround.xPosition -= dino.getDinoSpeedX();
-		float previousPosX = firstGround.xPosition;
+		firstGround.setXPosition(firstGround.getXPosition() - dino.getDinoSpeedX());
+		float previousPosX = firstGround.getXPosition();
 		while (itr.hasNext()) {
 			ImageGround element = itr.next();
-			element.xPosition = previousPosX + ground1.getWidth();
-			previousPosX = element.xPosition;
+			element.setXPosition(previousPosX + ground1.getWidth());
+			previousPosX = element.getXPosition();
 		}
-		if (firstGround.xPosition < -ground1.getWidth()) {
+		if (firstGround.getXPosition() < -ground1.getWidth()) {
 			listGround.remove(firstGround);
-			firstGround.xPosition = previousPosX + ground1.getWidth();
+			firstGround.setXPosition(previousPosX + ground1.getWidth());
 			setImageGround(firstGround);
 			listGround.add(firstGround);
+		}
+	}
+
+	public void draw(Graphics g) {
+		for (ImageGround imgGround : listGround) {
+			g.drawImage(imgGround.getImage(), (int) imgGround.getXPosition(), GROUND_Y_POS, null);
 		}
 	}
 
 	private void setImageGround(ImageGround imgGround) {
 		int typeGround = getTypeOfGround();
 		if (typeGround == 1) {
-			imgGround.image = ground1;
+			imgGround.setImage(ground1);
 		} else if (typeGround == 3) {
-			imgGround.image = ground3;
+			imgGround.setImage(ground3);
 		} else {
-			imgGround.image = ground2;
-		}
-	}
-
-	public void draw(Graphics g) {
-		for (ImageGround imgGround : listGround) {
-			g.drawImage(imgGround.image, (int) imgGround.xPosition, GROUND_Y_POS, null);
+			imgGround.setImage(ground2);
 		}
 	}
 
@@ -76,15 +76,30 @@ public class Ground {
 		if (type == 1) {
 			return 1;
 		} else if (type == 9) {
-			return 3;
-		} else {
 			return 2;
+		} else {
+			return 3;
 		}
 	}
 
 	private class ImageGround {
-		float xPosition;
-		BufferedImage image;
-	}
+		private float xPosition;
+		private BufferedImage image;
 
+		public float getXPosition() {
+			return xPosition;
+		}
+
+		public void setXPosition(float xPosition) {
+			this.xPosition = xPosition;
+		}
+
+		public BufferedImage getImage() {
+			return image;
+		}
+
+		public void setImage(BufferedImage image) {
+			this.image = image;
+		}
+	}
 }
